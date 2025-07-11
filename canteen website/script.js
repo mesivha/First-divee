36cd2d3c11e67d39d0df858a6b31115cef9c49ff
mesivha <mesivha0108@gmail.com>
2025-07-11 20:36:35 +0530

    Add files via upload

---
+++ canteen website/script.js
@@ -0,0 +1,15 @@
+let user = document.querySelector(".user");
+let pass = document.querySelector(".pass");
+
+let userName = "2023B1541212";
+let password = "2300321540071";
+
+let form = document.querySelector("#registrationForm");
+form.addEventListener("submit", (event) => {
+    event.preventDefault(); // Prevent default form submission
+    if (userName === user.value && password === pass.value) {
+        window.location.href = "http://127.0.0.1:5500/Web%20Dev/canteen%20website/index1.html";
+    } else {
+        alert("Invalid Username or Password");
+    }
+});
